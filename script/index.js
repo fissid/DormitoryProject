47feb729e55a089314c37ea070c2f115d2d8c666
Saeed Salehi <fissid.ss@gmail.com>
2023-07-03 19:45:59 +0300

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,5 @@
+document.querySelector(".devam").addEventListener("click", (e) => {
+  const email = document.querySelector(".email").value;
+  const pass = document.querySelector(".pass").value;
+  e.preventDefault();
+});
